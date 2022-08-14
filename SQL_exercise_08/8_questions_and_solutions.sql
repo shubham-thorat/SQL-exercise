69cd7d1f37a7cfb7602bf968c7994c3ee341c9ad
Abhijeet Patil <72167220+indtheblacktiger@users.noreply.github.com>
2022-08-14 20:15:29 +0530

    Update 8_questions_and_solutions.sql
    Added solution

--- SQL_exercise_08/8_questions_and_solutions.sql
+++ SQL_exercise_08/8_questions_and_solutions.sql
@@ -37,3 +37,6 @@
 -- The patient has undergone a procedure with a cost larger that $5,000
 -- The patient has had at least two appointment where the nurse who prepped the appointment was a registered nurse.
 -- The patient's primary care physician is not the head of any department.
+
+SELECT E.Patient, F.Name, F.PCP, G.Name FROM (SELECT * FROM (SELECT Pres.Patient, Pres.Physician FROM Prescribes Pres, Patient Pat WHERE Pat.SSN = Pres.Patient AND Pat.PCP = Pres.Physician) A JOIN (SELECT U.Patient, COUNT(U.Procedures), SUM(Pro.Cost) FROM Undergoes U, Procedures Pro WHERE Pro.Code = U.Procedures GROUP BY U.Patient HAVING SUM(Pro.Cost) > 5000) B ON A.Patient = B.Patient
+JOIN (SELECT A.Patient, A.PrepNurse, COUNT(A.AppointmentID) FROM Appointment A LEFT JOIN Nurse N ON A.PrepNurse = N.EmployeeID WHERE N.Registered = 1 GROUP BY A.Patient HAVING COUNT(A.AppointmentID) >= 2) C ON B.Patient = C.Patient JOIN (SELECT Pat.SSN, Pat.PCP FROM Patient Pat, Department D WHERE Pat.PCP != D.Head GROUP BY Pat.SSN) D ON C.Patient = D.SSN) E, Patient F, Physician G WHERE E.Patient = F.SSN AND F.PCP = G.EmployeeID
